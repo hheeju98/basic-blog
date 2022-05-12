0ce46386d4d2c5aeea9daddb0ab60d3379dabbdf
hheeju98 <hheeju98@naver.com>
2022-05-12 23:27:10 +0900

    회원가입 완료

---
+++ src/main/java/site/metacoding/blogproject/Controller/api/UserApiController.java
@@ -0,0 +1,28 @@
+package site.metacoding.blogproject.Controller.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import site.metacoding.blogproject.Controller.dto.ResponseDto;
+import site.metacoding.blogproject.domain.RoleType;
+import site.metacoding.blogproject.domain.User;
+import site.metacoding.blogproject.service.UserService;
+
+@RestController
+public class UserApiController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/api/user")
+    public ResponseDto<Integer> save(@RequestBody User user) { // username, password, email
+        System.out.println("UserApiController : save 호출됨");
+        // 실제로 DB에 insert하고 아래에서 return이 되면 된다.
+        user.setRole(RoleType.USER);
+        int result = userService.회원가입(user);
+        return new ResponseDto<Integer>(HttpStatus.OK, result); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+    }
+}
---
+++ src/main/java/site/metacoding/blogproject/Controller/dto/ResponseDto.java
@@ -0,0 +1,17 @@
+package site.metacoding.blogproject.Controller.dto;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResponseDto<T> {
+    HttpStatus status;
+    T data;
+}
