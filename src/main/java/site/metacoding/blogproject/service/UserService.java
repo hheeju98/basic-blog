9df54f4f807057faee06d7ca2fb9d3b83b6ba9c4
hheeju98 <hheeju98@naver.com>
2022-05-16 23:17:51 +0900

    비밀번호 해쉬 후 회원가입

--- src/main/java/site/metacoding/blogproject/service/UserService.java
+++ src/main/java/site/metacoding/blogproject/service/UserService.java
@@ -1,10 +1,12 @@
 package site.metacoding.blogproject.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogproject.domain.RoleType;
 import site.metacoding.blogproject.domain.User;
 import site.metacoding.blogproject.domain.UserRepository;
 
@@ -16,18 +18,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     @Transactional
     public void 회원가입(User user) {
+        String rawPassword = user.getPassword(); // 1234원문
+        String encPassword = encoder.encode(rawPassword); // 해쉬화
+        user.setPassword(encPassword);
+        user.setRole(RoleType.USER);
         userRepository.save(user);
     }
-
-    /**
-     * @Transactional(readOnly = true) // select할때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료 (정합성 유지
-     *                         가능해짐 )
-     *                         public User 로그인(User user) {
-     *                         return
-     *                         userRepository.findByUsernameAndPassword(user.getUsername(),
-     *                         user.getPassword());
-     *                         }
-     */
 }
